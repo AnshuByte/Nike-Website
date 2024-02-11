7fe2ef56543399e20fc283cc7a885b113b669c31
AnshuByte <anshurauniyar2000@gmail.com>
2024-02-11 12:07:38 +0530

    update sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
 const App = () => (
   <main className="relative ">
     nav
-    <section className="xl:padding-1 wide:padding-r padding-b">hero</section>
-    <section className="padding ">Popular products</section>
-    <section className="padding">SuperQuality</section>
-    <section className="padding-x py-10">Services</section>
-    <section className="padding">SpecialOffers</section>
-    <section className="bg-pale-blue padding ">CustomerReviews</section>
-    <section className="padding">Subscribe</section>
-    <section className="padding">SpecialOffers</section>
+    <section className="xl:padding-1 wide:padding-r padding-b ">
+      Hero
+    </section>
+    <section className="padding ">
+     PopularProducts
+    </section>
+    <section className="padding">
+     SuperQuality
+    </section>
+    <section className="padding-x py-10">
+      Services
+    </section>
+    <section className="padding">
+      SpecialOffers
+    </section>
+    <section className="bg-blue-50 padding ">
+      CustomerReviews
+    </section>
+    <section className="padding-x sm:py-32 py-16 w-full">
+      Subscribe
+    </section>
+    <section className="padding-x padding-t pb-9 bg-black">
+      Footer
+    </section>
   </main>
 );
   
